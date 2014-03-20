3aabf2510236f278e1686c2769123d07be1c9990
Gaurav Babu <gauravbabu74@gmail.com>
2014-03-20 05:23:28 -0500

    Changes made by Gaurav Babu (gauravbabu74@gmail.com).

--- Bizz2Credit/scripts/homepage.js
+++ Bizz2Credit/scripts/homepage.js
@@ -6,6 +6,35 @@
         
         title:'test',
         description:'gaurav',
+        detailsButtonText:"",
+        dashboardRefresh:function()
+        {
+            var dataSource = new kendo.data.DataSource({
+            transport: {
+            read: {
+                    url: "http://biz2services.com/mobapp/api/user/",
+                    type:"POST",
+                    dataType: "json", // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
+                    data: { apiaction:"userdashboard",userid:12516} // search for tweets that contain "html5"
+            }
+            },
+            schema: {
+                data: function(data)
+            	{
+                	return [data];
+                    
+            	}
+            }
+            });
+            dataSource.fetch(function(){
+
+				
+                    detailsButtonText = "Start an Application";
+                    
+                
+			}); 
+            detailsButtonText = "Start an Application";
+        },
         
         
     });
@@ -15,11 +44,11 @@
          {
              console.log(msdata);
              $.each(msdata, function( index, value ) {
-  			if(value.statusid >= 1){
-                  return true;
+  			if(value.statusid > 1){
+                  return false;
               }
 			});
-             return false;
+             return true;
          },
          initHome: function () {
             app.loginService.viewModel.showloder();
@@ -42,15 +71,19 @@
             dataSource.fetch(function(){
                 var that = this;
             	var data = that.data(); 
-                
+                var detailsHeader = 'test';
+                var detailsDescription;
+                var detailsButtonText;
+                var detailsButtonLink;
                 var cntGetStarted = data[0]['results']['data']['cntGetStarted'];
-                var loan_total = data[0]['results']['data']['loanamt'];
+                var loan_total = data[0]['results']['data']['loan']['total'];
+                var loan_ended = data[0]['results']['data']['loan']['ended'];
+                var loan_posted = data[0]['results']['data']['loan']['posted'];
                 var matchstatus = data[0]['results']['data']['matchstatus'];
                 var funded = data[0]['results']['data']['funded'];
+                var totmatch = data[0]['results']['data']['loan']['matches'];
 				//console.log(data);
-                //console.log(loan_total);
-                //console.log(matchstatus);
-               // console.log(app);
+                //console.log(app.homesetting.checkMatchesStatus(data[0]['results']['data']['loan']['matchrows']));
                 
                 if(cntGetStarted === 0 && loan_total === 0){
 					pos = 0;
@@ -71,11 +104,17 @@
 					pos = 4;
 				}  
                 //alert(pos);
-                console.log(pos);
+                //console.log(pos);
                 $('#stps ul li').removeClass();
                 $('#stps ul li:eq('+pos+')').addClass('activ');
                 $('#stps ul li:lt('+pos+')').addClass('dn');
                 //alert(pos);
+                if((cntGetStarted>=1 && loan_total===0) || (loan_total===loan_ended)) {
+                    detailsHeader ='Hi '+$username+', we have '+totmatch+' potential options for you!';
+                    detailsDescription='Please start your application in order to get matched to pre-qualified funding opportunities';
+                    detailsButtonText = "Start an Application";
+                    detailsButtonLink ="#";
+				}
                 app.loginService.viewModel.hideloder();
 			});      
         },
