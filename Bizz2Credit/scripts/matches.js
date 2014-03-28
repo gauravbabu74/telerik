6f1ed5b61bf6640143f33d888ace3c002314b538
Gaurav Babu <gauravbabu74@gmail.com>
2014-03-28 07:22:44 -0500

    Changes made by Gaurav Babu (gauravbabu74@gmail.com).

--- Bizz2Credit/scripts/matches.js
+++ Bizz2Credit/scripts/matches.js
@@ -1,42 +1,48 @@
 (function (global,$) {
+    
     var MatchespageViewModel,
-        app = global.app = global.app || {};
-
+   app = global.app = global.app || {};
     MatchespageViewModel = kendo.observable({
-       
-       init: function () {
-        var matchesData = new kendo.data.DataSource({
+       Matches : [],
+       getMatches: function () {
+       app.loginService.viewModel.showloder();
+        var dataSource = new kendo.data.DataSource({
             transport: {
-                read: function(options) {
-                   //console.log(options);
-                    
-                var i = 0;
-                var max = i + 5;
-                var data = [];
-                for (; i < max; i ++) {
-                	data.unshift({ name: "record" + i, modified: +new Date() });
-                }
-                    //console.log(data);
-                	options.success(data);
-                    //console.log(options);
+                read: {
+                    url: "http://biz2services.com/mobapp/api/user/",
+                    type:"POST",
+                    dataType: "json", // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
+                    data: { apiaction:"userdashboard",userid:sessionStorage.getItem("userID")} // search for tweets that contain "html5"
                 }
             },
-           
+            schema: {
+                data: function(data)
+                {
+                    //console.log(data);
+                	return [data['results']['data']['loan']['matchrows']];
+                }
+            }
         });
-        matchesData.fetch(function(){
+        dataSource.fetch(function(){
             var that = this;
             var data = that.data();
-            //console.log(data);
-           
-           
-        });
-        
-        },
+            console.log(data);
+            MatchespageViewModel.setMatches(data);
+         });
+        	   
+        },    
+        setMatches: function(data)
+           { 
+               var that = this;
+               that.set("Matches", data['0']);
+               app.loginService.viewModel.hideloder();
+           },
+     
    
     });
-    app.matchsetting = {
+$.extend(window, {
 
-		viewModel: MatchespageViewModel,     	
-    };
- 
+		MatchespageViewModel: MatchespageViewModel
+	});
+    
 })(window,jQuery);
