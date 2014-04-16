bd0ea60c4c8eecbe6aad690c2a2c77b2083d331f
Ramit Arora <ravic@biz2credit.com>
2014-04-16 08:25:59 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2Credit/scripts/documents.js
+++ Biz2Credit/scripts/documents.js
@@ -7,8 +7,31 @@
         showfilter:false,
         innerPage:false,
         parentPage:'',
-        currentFolderId:'',
+        currentFolderId:0,
         showrefreshLoading:false,
+        listViewInit:function(e) {
+                e.view.element.find("#list-edit-listview").kendoTouch({ 
+                    filter: ">li",
+                    tap: function (e) {    
+                        if(!hold)
+                        {
+                        	apps.navigate('#tabstrip-docs?parent='+e.touch.currentTarget.id);
+                        }
+                    },
+                    touchstart: function (e) {
+                    	hold = false;
+                    },
+                    hold: function (e) {
+                	 hold = true;
+					 navigator.notification.vibrate(100);
+                     $("#tabstrip-folder-events").data("kendoMobileModalView").open();
+            		 $("#tabstrip-folder-events").find(".km-scroll-container").css("-webkit-transform", "");
+                     $('#tabstrip-folder-events .folderName').html('');
+                     $('#tabstrip-folder-events .folderName').append('<span>'+e.touch.currentTarget.innerText+'</span>');
+                     $('#tabstrip-folder-events .folderName').attr("id",e.touch.currentTarget.id)
+                    }                    
+            });
+        },
 		documentShow:function(e)
         { 
             app.loginService.viewModel.showloder();
@@ -16,12 +39,14 @@
             {
                 var parentId = e.view.params.parent;
                 app.documentsetting.viewModel.setInnerPage();
+                app.documentsetting.viewModel.setcurrentFolderId(e.view.params.parent);
                 
             }
             else
             {
                 var parentId = 0;
                 app.documentsetting.viewModel.setMainPage();
+                app.documentsetting.viewModel.setcurrentFolderId(0);
             }
         
             
@@ -65,7 +90,7 @@
                         {
                         	docsArray.unshift(sharedFiles,sharedFolders);
                         }
-                        console.log(data);
+                        //console.log(data);
                     }
                 	return [docsArray];
                 }
@@ -123,9 +148,22 @@
              
         },
         
-        refreshView:function()
+        refreshView:function(e)
         {
            //app.loginService.viewModel.showloder();
+            //console.log(app.documentsetting.viewModel.currentFolderId);
+             if( app.documentsetting.viewModel.currentFolderId === 0)
+            {
+                var parentId = 0;
+                
+                
+            }
+            else
+            {
+                var parentId =  app.documentsetting.viewModel.currentFolderId;
+                
+            }
+            console.log(parentId);
             var that = this;
             that.set("showrefreshLoading", true);
        	 var dataSource = new kendo.data.DataSource({
@@ -134,29 +172,37 @@
                     url: "http://biz2services.com/mobapp/api/folder/",
                     type:"POST",
                     dataType: "json", // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
-                    data: {apiaction:"getlistfilesfolders",userID:localStorage.getItem("userID"),parentID:"0"}  // search for tweets that contain "html5"
+                    data: {apiaction:"getlistfilesfolders",userID:localStorage.getItem("userID"),parentID:parentId}  // search for tweets that contain "html5"
                 }
             },
                  
             schema: {
-                data: function(data)
-                { 
-                    
-                    var docsArray = [];
-                    $.each( data['results']['DocLists'], function( i, val ) {
+                 data: function(data)
+                {   var docsArray = [];
+                    if(data['results']['faultcode']===1)
+                    {
                         
-                        
-                        if(data['results']['DocLists'][i]['name']==='Shared Files'){
-                            sharedFiles =val;
+                        var sharedFiles ="";
+                            var sharedFolders ="";
+                        $.each( data['results']['DocLists'], function( i, val ) {
+                            
+                            
+                            if(data['results']['DocLists'][i]['name']==='Shared Files'){
+                                 sharedFiles =val;
+                            }
+                            else if(data['results']['DocLists'][i]['name']==='Shared Folders' ){
+                                 sharedFolders =val;
+                            }
+                            else{
+                                docsArray.push(val);
+                            } 
+    					});
+                        if(sharedFiles !== '' && sharedFolders !=='')
+                        {
+                        	docsArray.unshift(sharedFiles,sharedFolders);
                         }
-                        else if(data['results']['DocLists'][i]['name']==='Shared Folders' ){
-                            sharedFolders =val;
-                        }
-                        else{
-                            docsArray.push(val);
-                        } 
-					});
-                    docsArray.unshift(sharedFiles,sharedFolders);
+                        //console.log(data);
+                    }
                 	return [docsArray];
                 }
             },
@@ -188,6 +234,11 @@
         {
             var that = this;
             that.set("showrefreshLoading", false);
+        },
+        setcurrentFolderId:function(id)
+        {
+            var that = this;
+            that.set("currentFolderId", id);
         }
     });
     app.documentsetting = {
