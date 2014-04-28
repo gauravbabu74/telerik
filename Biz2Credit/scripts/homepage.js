91095b8f5d9f93673dea63d8a2b07073a625818a
Ramit Arora <ravic@biz2credit.com>
2014-04-28 13:41:36 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2Credit/scripts/homepage.js
+++ Biz2Credit/scripts/homepage.js
@@ -19,193 +19,199 @@
         dButtonLink:(window.localStorage.getItem("dButtonLink") !== null) ?  localStorage.getItem("dButtonLink") : '',
         homeShow: function (e) { 
         if(!app.loginService.viewModel.checkConnection()){
+               
                 navigator.notification.confirm('No Active Connection Found.', function (confirmed) {
             	if (confirmed === true || confirmed === 1) {
+                   
             		app.homesetting.viewModel.homeShow();
             	}
 
             }, 'Connection Error?', 'Retry,Cancel');
         }
-        app.loginService.viewModel.showloder();
-            
-        var dataSource = new kendo.data.DataSource({
-            transport: {
-                read: {
-                    url: "http://biz2services.com/mobapp/api/user/",
-                    type:"POST",
-                    dataType: "json", // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
-                    data: { apiaction:"userdashboard",userid:localStorage.getItem("userID")} // search for tweets that contain "html5"
-                }
-            },
-            schema: {
-                data: function(data)
-                {
-                	return [data];
-                }
-            }
-        });
-        dataSource.fetch(function(){
-            var that = this;
-            var data = that.data();
-            //console.log(data);
-            app.homesetting.viewModel.setMatches(data['0']['results']['data']['loan']['matchrows']);
-            pos = 1;
-            var cntGetStarted = data[0]['results']['data']['cntGetStarted'];
-            var matchstatus = data[0]['results']['data']['matchstatus'];
-            var totmatch = data[0]['results']['data']['totmatch'];
-            var creditscore = data[0]['results']['data']['creditscore'];
-            var loan_total = data[0]['results']['data']['loan']['total'];
-            var loan_posted = data[0]['results']['data']['loan']['posted'];
-            var loan_ended = data[0]['results']['data']['loan']['ended'];
-            var loan_saved = data[0]['results']['data']['loan']['saved'];
-            var matches = data[0]['results']['data']['loan']['matches'];
-            var matchrows =data[0]['results']['data']['loan']['matchrows'];
-            var funded =data[0]['results']['data']['funded'];
-            var userName= localStorage.getItem("userFName");
-            
-        	if(cntGetStarted === 0 && loan_total === 0){
-            	pos = 1;
-               
-            }
-            if(matchstatus === 2 && loan_total > 0){
-            	pos = 2;
-               
-            }
-            if(matchstatus === 0){
-            	pos = 3;
+        else
+        {    
+            app.loginService.viewModel.showloder();
                 
-            }
-            if(matchstatus === 1 &&  app.homesetting.checkMatchesStatus(data[0]['results']['data']['loan']['matchrows'])){
-            	pos = 3;
+            var dataSource = new kendo.data.DataSource({
+                transport: {
+                    read: {
+                        url: "http://biz2services.com/mobapp/api/user/",
+                        type:"POST",
+                        dataType: "json", // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
+                        data: { apiaction:"userdashboard",userid:localStorage.getItem("userID")} // search for tweets that contain "html5"
+                    }
+                    
+                },
+                schema: {
+                    data: function(data)
+                    {
+                    	return [data];
+                    }
+                }
+            });
+            dataSource.fetch(function(){
+                var that = this;
+                var data = that.data();
+                //console.log(data);
+                app.homesetting.viewModel.setMatches(data['0']['results']['data']['loan']['matchrows']);
+                pos = 1;
+                var cntGetStarted = data[0]['results']['data']['cntGetStarted'];
+                var matchstatus = data[0]['results']['data']['matchstatus'];
+                var totmatch = data[0]['results']['data']['totmatch'];
+                var creditscore = data[0]['results']['data']['creditscore'];
+                var loan_total = data[0]['results']['data']['loan']['total'];
+                var loan_posted = data[0]['results']['data']['loan']['posted'];
+                var loan_ended = data[0]['results']['data']['loan']['ended'];
+                var loan_saved = data[0]['results']['data']['loan']['saved'];
+                var matches = data[0]['results']['data']['loan']['matches'];
+                var matchrows =data[0]['results']['data']['loan']['matchrows'];
+                var funded =data[0]['results']['data']['funded'];
+                var userName= localStorage.getItem("userFName");
                 
-            }else if(matchstatus === 1 && funded === 0){
-            	pos = 4;
-              
-            }
-            if(matchstatus === 1 && funded === 1){
-            	pos = 5;
-               
-            }  
+            	if(cntGetStarted === 0 && loan_total === 0){
+                	pos = 1;
+                   
+                }
+                if(matchstatus === 2 && loan_total > 0){
+                	pos = 2;
+                   
+                }
+                if(matchstatus === 0){
+                	pos = 3;
+                    
+                }
+                if(matchstatus === 1 &&  app.homesetting.checkMatchesStatus(data[0]['results']['data']['loan']['matchrows'])){
+                	pos = 3;
+                    
+                }else if(matchstatus === 1 && funded === 0){
+                	pos = 4;
+                  
+                }
+                if(matchstatus === 1 && funded === 1){
+                	pos = 5;
+                   
+                }  
 
-            if((cntGetStarted >= 1 && loan_posted === 0)){
-               
-                    if(totmatch === "0")
-                    { 
-                        
-                        if(creditscore.search("-")){
-                            var tempvar = creditscore.lastIndexOf("-");
-        		        	var substr = creditscore.substring(0,tempvar);
-                            totmatch = app.homesetting.viewModel.getTotalMatches(substr);
+                if((cntGetStarted >= 1 && loan_posted === 0)){
+                   
+                        if(totmatch === "0")
+                        { 
                             
+                            if(creditscore.search("-")){
+                                var tempvar = creditscore.lastIndexOf("-");
+            		        	var substr = creditscore.substring(0,tempvar);
+                                totmatch = app.homesetting.viewModel.getTotalMatches(substr);
+                                
+                            }
+                            else{
+                                totmatch = app.homesetting.viewModel.getTotalMatches(creditscore);
+                            }
+                            dHeader ='Hi '+userName+', we have '+totmatch+' potential options for you!';
                         }
-                        else{
-                            totmatch = app.homesetting.viewModel.getTotalMatches(creditscore);
-                        }
-                        dHeader ='Hi '+userName+', we have '+totmatch+' potential options for you!';
+                    	else
+                    	{
+                        	dHeader ='Hi '+userName+', we have '+totmatch+' potential options for you!';
+                    	}
+                    	
+                        dDescription='Please start your application in order to get matched to pre-qualified funding opportunities';
+                        dButtonText = "Start an Application";
+                        dButtonLink ="#tabstrip-mess-one";
                     }
-                	else
-                	{
-                    	dHeader ='Hi '+userName+', we have '+totmatch+' potential options for you!';
-                	}
-                	
-                    dDescription='Please start your application in order to get matched to pre-qualified funding opportunities';
-                    dButtonText = "Start an Application";
-                    dButtonLink ="#tabstrip-mess-one";
-                }
-           if((cntGetStarted>=1 && loan_total===0) || (loan_total===loan_ended)) {
-               
-                    if(totmatch === 0 || totmatch === '')
-                    {
+               if((cntGetStarted>=1 && loan_total===0) || (loan_total===loan_ended)) {
+                   
+                        if(totmatch === 0 || totmatch === '')
+                        {
+                            dHeader='Hi '+userName+', We have 1200+ lenders to finance your needs';
+                        }
+                        else
+                        {
+                            dHeader ='Hi '+userName+', we have '+totmatch+' potential options for you!'; 
+                        }
+                		
+                		dDescription='Please start your application in order to get matched to pre-qualified funding opportunities';
+                		dButtonText = "Start an Application";
+                		dButtonLink ="#tabstrip-mess-one";
+                    }
+                if(cntGetStarted===0 && loan_total===0) {
                         dHeader='Hi '+userName+', We have 1200+ lenders to finance your needs';
+                        dDescription='Please start your application in order to get matched to pre-qualified funding opportunities';
+                        dButtonText = "Start an Application";
+                        dButtonLink ="#tabstrip-mess-one";
+    				}
+               /* if(loan_total === loan_saved && loan_total>0) {
+    					dHeader= userName+', your loan application is incomplete.';
+                        dDescription='In order to see what loan offers you qualify for, you must finish the application. Please click to resume or schedule a call to receive help from a loan expert.';
+                        dButtonText = "Complete Application";
+                        dButtonLink ="#";
+                    }*/
+                if(loan_posted === 0 && loan_saved >= 1){
+                    	dHeader= userName+', your loan application is incomplete.';
+                        dDescription='In order to see what loan offers you qualify for, you must finish the application. Please click to resume or schedule a call to receive help from a loan expert.';
+                        dButtonText = "Complete Application";
+                        dButtonLink ="#tabstrip-mess-two";
                     }
-                    else
-                    {
-                        dHeader ='Hi '+userName+', we have '+totmatch+' potential options for you!'; 
+                if(matchstatus===0 && matches>=1) {alert('fi');
+                     dHeader= userName+', you have '+matches+' pre-qualified loan matches.';
+                     dDescription='Please review your matches and select your preferred financing option(s)';
+                     dButtonText = "Select a Loan Product";
+                     dButtonLink ="views/matches.html";
                     }
-            		
-            		dDescription='Please start your application in order to get matched to pre-qualified funding opportunities';
-            		dButtonText = "Start an Application";
-            		dButtonLink ="#tabstrip-mess-one";
+                if(matchstatus===0 && matches===0) { 
+                     dHeader= 'You have '+matches+' loan matches';
+                     dDescription='No worries! We are here to help you.Use BizAnalyzer to find ways to improve your business\'s finances and funding opportunities.';
+                     dButtonText = "Check your BizAnalyzer Score";
+                     dButtonLink ="#tabstrip-mess-third";
+                    }
+                if(matchstatus === 1 && app.homesetting.checkMatchesStatus(matchrows)){
+                     dHeader= userName+', you have '+matches+' pre-qualified loan matches.';
+                     dDescription='Please review your matches and select your preferred financing option(s)';
+                     dButtonText = "Select a Loan Product";
+                     dButtonLink ="views/matches.html";
+                    }
+                else if(matchstatus === 1 && funded === 0)
+                {
+                     dHeader= userName+', the below submissions are still pending.';
+                     dDescription= 'Please review these items and complete any remaining actions if necessary';
+                     dButtonText = app.homesetting.viewModel.getLatestMatchStatus(matchrows);
+                     dButtonLink ="views/matches.html";
                 }
-            if(cntGetStarted===0 && loan_total===0) {
-                    dHeader='Hi '+userName+', We have 1200+ lenders to finance your needs';
-                    dDescription='Please start your application in order to get matched to pre-qualified funding opportunities';
-                    dButtonText = "Start an Application";
-                    dButtonLink ="#tabstrip-mess-one";
-				}
-           /* if(loan_total === loan_saved && loan_total>0) {
-					dHeader= userName+', your loan application is incomplete.';
-                    dDescription='In order to see what loan offers you qualify for, you must finish the application. Please click to resume or schedule a call to receive help from a loan expert.';
-                    dButtonText = "Complete Application";
-                    dButtonLink ="#";
-                }*/
-            if(loan_posted === 0 && loan_saved >= 1){
-                	dHeader= userName+', your loan application is incomplete.';
-                    dDescription='In order to see what loan offers you qualify for, you must finish the application. Please click to resume or schedule a call to receive help from a loan expert.';
-                    dButtonText = "Complete Application";
-                    dButtonLink ="#tabstrip-mess-two";
-                }
-            if(matchstatus===0 && matches>=1) {alert('fi');
-                 dHeader= userName+', you have '+matches+' pre-qualified loan matches.';
-                 dDescription='Please review your matches and select your preferred financing option(s)';
-                 dButtonText = "Select a Loan Product";
-                 dButtonLink ="views/matches.html";
-                }
-            if(matchstatus===0 && matches===0) { 
-                 dHeader= 'You have '+matches+' loan matches';
-                 dDescription='No worries! We are here to help you.Use BizAnalyzer to find ways to improve your business\'s finances and funding opportunities.';
-                 dButtonText = "Check your BizAnalyzer Score";
-                 dButtonLink ="#tabstrip-mess-third";
-                }
-            if(matchstatus === 1 && app.homesetting.checkMatchesStatus(matchrows)){
-                 dHeader= userName+', you have '+matches+' pre-qualified loan matches.';
-                 dDescription='Please review your matches and select your preferred financing option(s)';
-                 dButtonText = "Select a Loan Product";
-                 dButtonLink ="views/matches.html";
-                }
-            else if(matchstatus === 1 && funded === 0)
-            {
-                 dHeader= userName+', the below submissions are still pending.';
-                 dDescription= 'Please review these items and complete any remaining actions if necessary';
-                 dButtonText = app.homesetting.viewModel.getLatestMatchStatus(matchrows);
-                 dButtonLink ="views/matches.html";
-            }
-            if(matchstatus === 1 && funded === 1){
-			
-				dHeader= userName+', the below submissions are still pending.';
-				dDescription= 'Please review these items and complete any remaining actions if necessary';
-            	dButtonText = app.homesetting.viewModel.getLatestMatchStatus(matchrows);
-            	dButtonLink ="views/matches.html";
+                if(matchstatus === 1 && funded === 1){
+    			
+    				dHeader= userName+', the below submissions are still pending.';
+    				dDescription= 'Please review these items and complete any remaining actions if necessary';
+                	dButtonText = app.homesetting.viewModel.getLatestMatchStatus(matchrows);
+                	dButtonLink ="views/matches.html";
+                    
+    		    }
                 
-		    }
-            
-            $('#stps ul li').removeClass();
-            if(pos===1)
-            {
-                 $('#stps ul li:eq('+(pos-1)+')').addClass('activ');
-                 $('#stps ul li:lt('+(pos)+')').addClass('dn');
-            }
-            else
-            {
-       		 $('#stps ul li:eq('+(pos-1)+')').addClass('activ');
-            	$('#stps ul li:lt('+(pos-1)+')').addClass('dn'); 
-            }
-           
-            
-            $("#home-call-btn").html("");
-            if(dButtonLink === "views/matches.html")
-            {
-                var html = '<a class="btngr" href="'+dButtonLink+'" data-role="button">'+dButtonText+'</a>';
-            }
-            else{
-                var html = '<a class="btngr" href="'+dButtonLink+'" data-rel="modalview" data-role="button">'+dButtonText+'</a>';
-            }
-             
-             $("#home-call-btn").append(html);
-             app.homesetting.viewModel.setHomeToolTips(data[0]['results']['data']);
-             app.homesetting.viewModel.setcache(dHeader,dDescription,dButtonText,dButtonLink);
-           
-        });    
+                $('#stps ul li').removeClass();
+                if(pos===1)
+                {
+                     $('#stps ul li:eq('+(pos-1)+')').addClass('activ');
+                     $('#stps ul li:lt('+(pos)+')').addClass('dn');
+                }
+                else
+                {
+           		 $('#stps ul li:eq('+(pos-1)+')').addClass('activ');
+                	$('#stps ul li:lt('+(pos-1)+')').addClass('dn'); 
+                }
+               
+                
+                $("#home-call-btn").html("");
+                if(dButtonLink === "views/matches.html")
+                {
+                    var html = '<a class="btngr" href="'+dButtonLink+'" data-role="button">'+dButtonText+'</a>';
+                }
+                else{
+                    var html = '<a class="btngr" href="'+dButtonLink+'" data-rel="modalview" data-role="button">'+dButtonText+'</a>';
+                }
+                 
+                 $("#home-call-btn").append(html);
+                 app.homesetting.viewModel.setHomeToolTips(data[0]['results']['data']);
+                 app.homesetting.viewModel.setcache(dHeader,dDescription,dButtonText,dButtonLink);
+               
+            });
+        }
         },       
 		setcache:function(dHeader,dDescription,dButtonText,dButtonLink)
         {
