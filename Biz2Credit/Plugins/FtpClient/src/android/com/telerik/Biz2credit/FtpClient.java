ffa1e234ba041a3f0057a85f973ed0c57ba9861b
Ramit Arora <ravic@biz2credit.com>
2014-05-07 07:47:56 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2Credit/Plugins/FtpClient/src/android/com/telerik/Biz2credit/FtpClient.java
+++ Biz2Credit/Plugins/FtpClient/src/android/com/telerik/Biz2credit/FtpClient.java
@@ -19,28 +19,225 @@ import android.os.Environment;
 import android.util.Base64;
 import android.util.Log;
 
-/**
- * Ftpclient.java
- *
- * Android implementation of the Canvas2ImagePlugin for iOS.
- * Inspirated by Joseph's "Save HTML5 Canvas Image to Gallery" plugin
- * http://jbkflex.wordpress.com/2013/06/19/save-html5-canvas-image-to-gallery-phonegap-android-plugin/
- *
- * @author Vegard LÃ¸kken <vegard@headspin.no>
+
+
+
+import android.os.Environment;
+
+import android.util.Log;
+
+
+import it.sauronsoftware.ftp4j.FTPAbortedException;
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferException;
+import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
+
+
+import java.io.IOException;
+
+
+
+
+
+import android.content.Context;
+
+import android.os.Handler;
+
+
+
+/* Copy write 23-12-2013
+ * Sakshay WebTechnology Pvt Ltd
+ * www.sakshay.in
  */
 public class FtpClient extends CordovaPlugin {
 	public static final String ACTION = "saveImageDataToLibrary";
+    private FTPClient ftpClient;
+	private long fileSize=0;
+	public static final String ACTION_DOWNLOAD = "downloadFile";
+	public static final String ACTION_UPLOAD = "uploadFile";
+//	boolean isConnected = false;
+	
+	public static final String SUCCESS = "success";
+	public static final String PARSINGERROR = "parsing error";
+	public static final String CONNEERROR = "conn error";
+	public static final String URIERROR = "uri error";
+	public static final String EXCEPTION = "exception";
+	public static final String FTPABORTEDEXCEPTION = "ftpabortedexception";
+	public static final String FTPDATATRANSFEREXCEPTION = "ftpdatattransferexception";
+	public static final String FTPEXCEPTION = "ftpexception";
+	public static final String FTPILLEGALREPLYEXCEPTION = "ftpillegalreplyexception";
+	public static final String IOEXCEPTION = "ioexception";
+	public static final String SOCKET_TIMEOUTEXCEPTION = "socket_timeout";
+	public static final String SOCKET_NOEXCEPTION = "socket_noexception";
+	
+	public static final String HOST = "107.21.114.127";// storagetest.in
+	public static final String USER_NAME = "b2cdocs";
+	public static final String PASSWORD = "4Lz}+u&ZiizD5o1y";
+	
 
 	@Override
 	public boolean execute(String action, JSONArray data,
-			CallbackContext callbackContext) throws JSONException {
+			final CallbackContext callbackContext) throws JSONException {
 
-            String user_id=data.optString(0);
- 		   callbackContext.success("id = "+user_id);
-            return true;
+           Handler h=new Handler();
+		   h.postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				String s=setFtpConnection();
+				if(s.equalsIgnoreCase(SOCKET_NOEXCEPTION)){
+					
+					callbackContext.success("Success");
+				}
+			}
+		}, 1);
+		
+		if (action.equals(ACTION_DOWNLOAD)) {
+			if(isConnected()){
+			String status=downloadFile("abc.pdf", "abc.pdf");
+			if(status.equalsIgnoreCase(SUCCESS)){
+				callbackContext.success("Success");
+			}
+			}
+			else
+				callbackContext.error("Connection to Server Failed");
+				
+		}
+		
+		return false;
       
 	}
 
-	
-	
+
+public String downloadFile(String serverFileName,
+			String localFileName) {
+		String result = "";
+		
+		// download in sdcard
+		File root = Environment.getExternalStorageDirectory();
+		File file = new File(root, localFileName);
+		try {
+			
+			// upar se uthana h (Gaurav sir)
+			String fullPath="/public_html/components/com_brief/files/12516";
+			Log.i("FTP full path : ", fullPath);
+			ftpClient.changeDirectory(fullPath);
+			ftpClient.download(serverFileName,file);
+			result = SUCCESS;
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			Log.i("FTP Exception", "IllegalStateException");
+			e.printStackTrace();
+			result = EXCEPTION;
+			disconnectFTP();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			Log.i("FTP Exception", "IOException");
+			e.printStackTrace();
+			result = IOEXCEPTION;
+			disconnectFTP();
+		} catch (FTPIllegalReplyException e) {
+			// TODO Auto-generated catch block
+			Log.i("FTP Exception", "FTPIllegalReplyException");
+			e.printStackTrace();
+			result = FTPILLEGALREPLYEXCEPTION;
+			disconnectFTP();
+		} catch (FTPException e) {
+			// TODO Auto-generated catch block
+			Log.i("FTP Exception", "FTPException");
+			e.printStackTrace();
+			result = FTPEXCEPTION;
+			disconnectFTP();
+		} catch (FTPDataTransferException e) {
+			// TODO Auto-generated catch block
+			Log.i("FTP Exception", "FTPDataTransferException");
+			e.printStackTrace();
+			result = FTPDATATRANSFEREXCEPTION;
+			disconnectFTP();
+		} catch (FTPAbortedException e) {
+			// TODO Auto-generated catch block
+			Log.i("FTP Exception", "FTPAbortedException");
+			e.printStackTrace();
+			result = FTPABORTEDEXCEPTION;
+			disconnectFTP();
+		} catch(Exception e){
+			// TODO Auto-generated catch block
+			Log.i("FTP Exception", "Exception");
+			e.printStackTrace();
+			result = FTPABORTEDEXCEPTION;
+			disconnectFTP();
+		}
+		return result;
+	}
+
+public String setFtpConnection() {
+		if (ftpClient == null)
+			ftpClient = new FTPClient();
+		String exception = SOCKET_NOEXCEPTION;
+		if (!ftpClient.isConnected()) {
+			try {
+				ftpClient.connect(HOST, 21);
+				ftpClient.login(USER_NAME, PASSWORD);
+				ftpClient.setType(FTPClient.TYPE_BINARY);
+				exception = SOCKET_NOEXCEPTION;
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				exception = FTPEXCEPTION;
+				disconnectFTP();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				disconnectFTP();
+				exception = SOCKET_TIMEOUTEXCEPTION;
+			} catch (FTPIllegalReplyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				exception = FTPEXCEPTION;
+				disconnectFTP();
+			} catch (FTPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				exception = FTPEXCEPTION;
+				disconnectFTP();
+			}
+		}
+		return exception;
+	}
+
+public void disconnectFTP() {
+		Log.i("FTP", "disconnectFTP method ");
+		if (ftpClient.isConnected()) {
+			try {
+				if (ftpClient.isAuthenticated()) {
+					ftpClient.logout();
+				}
+				ftpClient.disconnect(true);
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FTPIllegalReplyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (FTPException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		ftpClient = null;
+	}
+
+public boolean isConnected() {
+		boolean b = false;
+		if (ftpClient != null)
+			if (ftpClient.isAuthenticated() && ftpClient.isConnected())
+				b = true;
+		return b;
+	}
+
 }
