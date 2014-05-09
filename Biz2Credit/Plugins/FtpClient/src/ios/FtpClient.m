38805a8d21a399b84ae6bdacab501b32ef90f462
Ramit Arora <ravic@biz2credit.com>
2014-05-09 02:30:53 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2Credit/Plugins/FtpClient/src/ios/FtpClient.m
+++ Biz2Credit/Plugins/FtpClient/src/ios/FtpClient.m
@@ -13,28 +13,9 @@
 @implementation FtpClient
 @synthesize callbackId;
 
-//-(CDVPlugin*) initWithWebView:(UIWebView*)theWebView
-//{
-//    self = (Canvas2ImagePlugin*)[super initWithWebView:theWebView];
-//    return self;
-//}
 
 
-//- (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command
-- (void)downloadFile:(CDVInvokedUrlCommand*)command
-{
 
-    ///  NSLog(@"command %@",command.arguments);
-     UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"Message" message:@"Abhishek msg" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles: nil] ;
-        [alert show];
-        
-   // self.callbackId = command.callbackId;
-//	NSData* imageData = [NSData dataFromBase64String:[command.arguments objectAtIndex:0]];
-	
-	//UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];	
-	//UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-	
-}
 
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
 {
@@ -60,6 +41,141 @@
 	[callbackId release];
     [super dealloc];
 }
+
+- (void)downloadFile:(CDVInvokedUrlCommand*)command
+{
+    UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"Message" message:@"Abhishek msg" delegate:nil cancelButtonTitle:@"ok" otherButtonTitles: nil] ;
+        [alert show];
+    recievedCommand = command;
+    
+    [self startDownloadFile:command.arguments];
+}
+
+-(void)startDownloadFile:(NSArray*)arrOfParams
+{
+    
+  
+    downloadFile = [[BRRequestDownload alloc] initWithDelegate:self];
+    //the path needs to be absolute to the FTP root folder.
+    //full URL would be ftp://xxx.xxx.xxx.xxx/space.jpg
+   
+    NSString *downloadingPath;
+    NSString *Hostname;
+    NSString *Username;
+    NSString *Password;
+    
+    if (arrOfParams!=nil) {
+        downloadingPath = [arrOfParams objectAtIndex:0];
+        Hostname = [arrOfParams objectAtIndex:1];
+        Username = [arrOfParams objectAtIndex:2];
+        Password = [arrOfParams objectAtIndex:3];
+    }
+    
+    downloadData = [[NSMutableData alloc]init];
+    downloadFile.path = @"/public_html/components/com_brief/files/12516/2243.file";
+    NSArray *arr = [downloadFile.path componentsSeparatedByString:@"/"];
+    fileName = [arr lastObject];
+    [downloadFile setHostname:@"107.21.114.127"];
+    [downloadFile setUsername:@"b2cdocs"];
+    
+    [downloadFile setPassword:@"NEx6fSt1JlppaXpENW8xeQ=="];
+    
+    
+    //we start the request
+    [downloadFile start];
+    
+    
+}
+
+- (long) requestDataSendSize: (BRRequestUpload *) request
+{
+    //----- user returns the total size of data to send. Used ONLY for percentComplete
+    return [uploadData length];
+    
+}
+
+- (NSData *)requestDataToSend:(BRRequestUpload *) request
+{
+    //----- returns data object or nil when complete
+    //----- basically, first time we return the pointer to the NSData.
+    //----- and BR will upload the data.
+    //----- Second time we return nil which means no more data to send
+   
+    NSData *temp = uploadData;
+    
+    // this is a shallow copy of the pointer, not a deep copy
+    
+    uploadData = nil; // next time around, return nil...
+    
+    return temp;
+}
+
+#pragma mark
+#pragma mark ftp White Raccon delegates  Methods
+
+- (void) requestDataAvailable: (BRRequestDownload *) request;
+{
+    [downloadData appendData:request.receivedData];
+    
+}
+-(void) requestCompleted: (BRRequest *) request
+{
+    
+    NSLog(@"%@ completed!", request);
+    
+    if (request == downloadFile)
+    {
+        NSLog(@"%@ completed!", request);
+        
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *documentsDirectory = [paths objectAtIndex:0];
+        NSString *tempFolderPath = [documentsDirectory stringByAppendingPathComponent:@"Downloads"];
+        
+        [[NSFileManager defaultManager] createDirectoryAtPath:tempFolderPath withIntermediateDirectories:YES attributes:nil error:NULL];
+        tempFolderPath = [tempFolderPath stringByAppendingPathComponent:fileName];
+        
+        if (![[NSFileManager defaultManager] fileExistsAtPath:tempFolderPath]) {
+            [[NSFileManager defaultManager] createFileAtPath:tempFolderPath contents:nil attributes:nil];
+        }
+        BOOL success;
+        success  = [downloadData writeToFile:tempFolderPath atomically:YES];
+        
+        if (success) {
+            NSDictionary *jsonObj =[[NSDictionary alloc]initWithObjectsAndKeys:tempFolderPath, @"savedFilePath",@"true", @"success",nil];
+            CDVPluginResult *pluginResult =[CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsDictionary :jsonObj];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:recievedCommand.callbackId];
+        }else{
+            NSDictionary *jsonObj =[[NSDictionary alloc]initWithObjectsAndKeys:@"", @"savedFilePath",@"false", @"success",nil];
+            CDVPluginResult *pluginResult =[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary :jsonObj];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:recievedCommand.callbackId];
+        
+        }
+        
+    }
+    
+}
+
+- (void) percentCompleted: (BRRequest *) request
+{
+    NSLog(@"%f completed...",request.percentCompleted);
+    NSLog(@"%ld bytes this iteration", request.bytesSent);
+    NSLog(@"%ld total bytes",request.totalBytesSent);
+    
+}
+
+-(void) requestFailed:(BRRequest *) request
+{
+    
+    NSLog(@"%@",request.error.message);
+    NSDictionary *jsonObj =[[NSDictionary alloc]initWithObjectsAndKeys:request.error.message, @"savedFilePath",@"false", @"success",nil];
+    CDVPluginResult *pluginResult =[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary :jsonObj];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:recievedCommand.callbackId];
+    
+    UIAlertView *alert=[[UIAlertView alloc] initWithTitle:@"Message" message:request.error.message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil] ;
+    [alert show];
+    
+}
+
 
 
 @end
