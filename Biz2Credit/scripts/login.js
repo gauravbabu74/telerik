38e85d5a402ee151b7398573e3b4520b9274a1f2
Ramit Arora <ravic@biz2credit.com>
2014-05-12 00:38:14 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2Credit/scripts/login.js
+++ Biz2Credit/scripts/login.js
@@ -144,20 +144,24 @@
         
         checkConnection:function()
         {
-            /*var networkState = navigator.connection.type;
-            var states = {};
-            states[Connection.UNKNOWN] = 'Unknown connection';
-            states[Connection.ETHERNET] = 'Ethernet connection';
-            states[Connection.WIFI] = 'WiFi connection';
-            states[Connection.CELL_2G] = 'Cell 2G connection';
-            states[Connection.CELL_3G] = 'Cell 3G connection';
-            states[Connection.CELL_4G] = 'Cell 4G connection';
-            states[Connection.CELL] = 'Cell generic connection';
-            states[Connection.NONE] = 'No network connection';
-            if (states[networkState] === 'No network connection') {
-                //navigator.notification.alert('No active connection found!');
-                return false;
-            }*/
+            if(typeof navigator.connection.type !== "undefined")
+            {
+                var networkState = navigator.connection.type;
+                var states = {};
+                states[Connection.UNKNOWN] = 'Unknown connection';
+                states[Connection.ETHERNET] = 'Ethernet connection';
+                states[Connection.WIFI] = 'WiFi connection';
+                states[Connection.CELL_2G] = 'Cell 2G connection';
+                states[Connection.CELL_3G] = 'Cell 3G connection';
+                states[Connection.CELL_4G] = 'Cell 4G connection';
+                states[Connection.CELL] = 'Cell generic connection';
+                states[Connection.NONE] = 'No network connection';
+                if (states[networkState] === 'No network connection') {
+                    //navigator.notification.alert('No active connection found!');
+                    return false;
+                }
+            }
+            
             return true;
         },
         checkConnectionRetrycall:function(e)
