b2e354a4f46974bdd87114f36a4cb87bd89a7072
Ramit Arora <ravic@biz2credit.com>
2014-05-16 03:35:28 -0500

    Changes made by Ramit Arora (ravic@biz2credit.com).

--- Biz2Credit/scripts/documents.js
+++ Biz2Credit/scripts/documents.js
@@ -849,15 +849,16 @@
         },
         closeFileDownloadProcess:function()
         {
-           $("#tabstrip-download-file").data("kendoMobileModalView").close();
+            alert("call");
+          // $("#tabstrip-download-file").data("kendoMobileModalView").close();
            //if (device.platform.toLowerCase() === "ios") {
-            navigator.notification.confirm('Do you really want to exit?', function (confirmed) {
+           /* navigator.notification.confirm('Do you really want to exit?', function (confirmed) {
 				if (confirmed === true || confirmed === 1) {
                	$("#tabstrip-download-file").data("kendoMobileModalView").close();
             	   app.documentsetting.viewModel.transferFileAbort();
             	}
                 
-        	}, 'exit', 'Ok,Cancel');
+        	}, 'exit', 'Ok,Cancel');*/
            // }
         },
         
